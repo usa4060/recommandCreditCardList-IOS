1d94882eccefaa46706f1b5076489c6dbbe54d79
usa4060 <cocoa9805@naver.com>
2022-03-08 23:50:05 +0900

    Recommand Credit Card List

--- RecommandCreditCardList/RecommandCreditCardList/CardListViewController.swift
+++ RecommandCreditCardList/RecommandCreditCardList/CardListViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 import Kingfisher
-
+import FirebaseDatabase
 class CardListViewController : UITableViewController {
+    var ref : DatabaseReference! // Firebase Realtime Database를 가져올 수 있는 레퍼런스 값
+    
     var creditCardList : [CreditCard] = []
     
     override func viewDidLoad() {
@@ -11,6 +13,33 @@ class CardListViewController : UITableViewController {
         let nibname = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(nibname, forCellReuseIdentifier: "CardListCell")
         
+        // Firebase가 우리의 Database를 잡아내고, Firebase의 Realtime Database와 주고 받게 된다.
+        ref = Database.database().reference()
+        
+        // observe를 통해 Realtime Database의 값을 가져오게 되는데 snapshot이라는 변수로 가져오게 된다.
+        ref.observe(.value) { snapshot in
+            // 가져오는 value는 Dictionary의 형태로 가져오게 된다 -> as? [ : [ : ]]
+            guard let value = snapshot.value as? [String : [String: Any]] else {return}
+            
+            // 가져욘 value를 JSONDecoding 하게 되는데, 이는 try문의 형태로 주어진다.
+            do {
+                // Database의 자료를 JSON형식으로 변환
+                let jsonData = try JSONSerialization.data(withJSONObject: value)
+                // JSON형식으로 변환된 Data를 [String : CreditCard]의 형식으로 변환
+                let cardData = try JSONDecoder().decode([String : CreditCard].self, from: jsonData)
+                // 각 cardData의 값들을 Array형식으로 변환 및 정렬
+                let cardList = try Array(cardData.values)
+                self.creditCardList = cardList.sorted{$0.rank < $1.rank}
+                
+                // UI는 메인 쓰레드에서 그려야하기 때문에 DispatchQueue를 사용
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            catch let error{
+                print("ERROR JSON parsing \(error.localizedDescription)")
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
