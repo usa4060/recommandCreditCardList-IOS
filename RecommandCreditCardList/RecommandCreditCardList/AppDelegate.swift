4b5d4840a3d8541eb1730c91307c1397186289c7
usa4060 <cocoa9805@naver.com>
2022-03-15 19:10:04 +0900

    Recommand Credit Card List

--- RecommandCreditCardList/RecommandCreditCardList/AppDelegate.swift
+++ RecommandCreditCardList/RecommandCreditCardList/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestoreSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         //firebase 등록
         FirebaseApp.configure()
+        
+        let db = Firestore.firestore()
+        
+        // 처음 실행할 때, document가 존재하는지 확인하기 위해 .getDocuments를 이용해서 snapshot을 가져온다.
+        db.collection("creditCardList").getDocuments { snapshot, _ in
+            guard snapshot?.isEmpty == true else {return}
+            let batch = db.batch()
+            
+            // creditCardList라는 collection에 card0라는 경로를 만들어 준 것이다.
+            // 동일하게 10개 만들기
+            let card0Ref = db.collection("creditCardList").document("card0")
+            let card1Ref = db.collection("creditCardList").document("card1")
+            let card2Ref = db.collection("creditCardList").document("card2")
+            let card3Ref = db.collection("creditCardList").document("card3")
+            let card4Ref = db.collection("creditCardList").document("card4")
+            let card5Ref = db.collection("creditCardList").document("card5")
+            let card6Ref = db.collection("creditCardList").document("card6")
+            let card7Ref = db.collection("creditCardList").document("card7")
+            let card8Ref = db.collection("creditCardList").document("card8")
+            let card9Ref = db.collection("creditCardList").document("card9")
+            
+            // batch에 넣는 setData라는 함수가 throw함수 이기때문에 do ~ catch 문으로 작성
+            do{
+                try batch.setData(from: CreditCardDummy.card0, forDocument: card0Ref)
+                try batch.setData(from: CreditCardDummy.card1, forDocument: card1Ref)
+                try batch.setData(from: CreditCardDummy.card2, forDocument: card2Ref)
+                try batch.setData(from: CreditCardDummy.card3, forDocument: card3Ref)
+                try batch.setData(from: CreditCardDummy.card4, forDocument: card4Ref)
+                try batch.setData(from: CreditCardDummy.card5, forDocument: card5Ref)
+                try batch.setData(from: CreditCardDummy.card6, forDocument: card6Ref)
+                try batch.setData(from: CreditCardDummy.card7, forDocument: card7Ref)
+                try batch.setData(from: CreditCardDummy.card8, forDocument: card8Ref)
+                try batch.setData(from: CreditCardDummy.card9, forDocument: card9Ref)
+            }
+            catch let error{
+                print("ERROR writing card to firebase \(error.localizedDescription)")
+            }
+            
+            // 반드시 batch.commit을 해 주어야 데이터가 추가가 된다.
+            batch.commit()
+        }
         return true
     }
 
