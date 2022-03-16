c415a93c6ada77e795bc4baa53e3e580262a24bd
usa4060 <cocoa9805@naver.com>
2022-03-16 23:52:12 +0900

    Recommand Credit Card

--- RecommandCreditCardList/RecommandCreditCardList/CreditCardDummy.swift
+++ RecommandCreditCardList/RecommandCreditCardList/CreditCardDummy.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// 오류 해결 필요.... 2시간째 해결 못함,,,, ㅜ
 struct CreditCardDummy {
     static let card0 = CreditCard(id: 0, rank: 1, name: "신한카드", cardImageURL: "https://www.shinhancard.com/_ICSFiles/afieldfile/2019/04/26/190426_pc_mrlife_cardplate600x380.png", promotionDetail: PromotionDetail(companyName: "신한", period: "2023.01.07(목)~2023.01.31(토)", amount: 13, condition: "온라인 채널을 통해 이벤트 카드를 보유하고, 혜택조건을 충족하신 분", benefitCondition: "이벤트 카드로 결제한 금액이 합해서 10만원이상 결제", benefitDetail: "현금 10만원", benefitDate: "2023.03.01(월)이후"), isSelected: nil)
     static let card1 = CreditCard(id: 1, rank: 2, name: "taptap S", cardImageURL: "https://static11.samsungcard.com/wcms/scard/personal/__icsFiles/afieldfile/2019/06/12/AAP1482_s.png", promotionDetail: PromotionDetail(companyName: "삼성", period: "2023.01.07(목)~2023.01.31(토)", amount: 12, condition: "온라인 채널을 통해 이벤트 카드를 보유하고, 혜택조건을 충족하신 분", benefitCondition: "이벤트 카드로 결제한 금액이 합해서 10만원이상 결제", benefitDetail: "포인트 10만원", benefitDate: "2023.03.01(월)이후"), isSelected: nil)
